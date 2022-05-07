479484b8581f9bf7f01a4dcc24cd9c3ea821e2cf
Ryan <rksmith2942@gmail.com>
2022-05-07 15:56:06 -0400

    commerical calc function working, layour issues persist

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
-var resPlum;
+var Plum;
 var costConstructfee = 14;
 var costConstrt = 500;
 var fixFee = 50;
-//test
+
 window.onload = function()  // sets focus on first box, submit button
 {
     document.getElementById("resplum").focus();
     document.getElementById("sbmt").onclick = getValResPlumb;
+    document.getElementById("sbmt2").onclick = getValCommPlumb;
 };
 
 
 function getValResPlumb() //gets the value in the input box and puts it in resPlum
 {
-    resPlum = document.getElementById("resplum").value;
+    Plum = document.getElementById("resplum").value;
 
     document.getElementById("fixfee").value = "$" + resFeeCalc()[0];
     document.getElementById("confee").value = "$" + resFeeCalc()[1];
@@ -23,13 +24,29 @@ function getValResPlumb() //gets the value in the input box and puts it in resPl
 
 };
 
-function resFeeCalc() //calculates the fees and returns as array 
-{
-    if (resPlum != 0){
-        var fixtureFee = resPlum * fixFee; //calculates the fee for the fixtures
-        var conFee = Math.ceil(((resPlum * costConstrt + 500) / 1000)) * costConstructfee; // calculates the cost of construction the 500 is for the first fixture being $1000 cost
-        var totalFee = conFee + fixtureFee; //total fee
+function getValCommPlumb(){
+    Plum = document.getElementById("complum").value;
+    document.getElementById("fixfeecomm").value = "$" + (resFeeCalc(1)[0]);
+    document.getElementById("confeecomm").value = "$" + resFeeCalc(1)[1];
+    document.getElementById("feecomm").value = "$" + resFeeCalc(1)[2];
+    document.getElementById("mainfixfeecomm").value = "$" + (resFeeCalc(1)[0] * 2);
+    document.getElementById("mainconfeecomm").value = "$" + (resFeeCalc(1)[1] * 2);
+    document.getElementById("mainfeecomm").value = "$" + (resFeeCalc(1)[2] * 2);
 
+
+};
+
+function resFeeCalc(comm) //calculates the fees and returns as array 
+{
+    if (Plum != 0){
+        var conFee = Math.ceil(((Plum * costConstrt + 500) / 1000)) * costConstructfee; // calculates the cost of construction the 500 is for the first fixture being $1000 cost
+
+        if (comm != 1){
+            var fixtureFee = Plum * fixFee; //calculates the fee for the fixtures for residential
+        } else{
+            fixtureFee = Plum * fixFee + 50 //for commercial with an extra 50 for the first fixture
+        }
+        var totalFee = conFee + fixtureFee; //total fee
     } else{
         return [0, 0, 0]
     }
