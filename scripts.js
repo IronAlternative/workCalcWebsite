fb31892d85855885673c6c3049f998138961f71a
Ryan <rksmith2942@gmail.com>
2022-05-07 10:51:29 -0400

    calculator function for residential added, fixed reset on form

--- scripts.js
+++ scripts.js
@@ -2,22 +2,37 @@ var resPlum;
 var costConstructfee = 14;
 var costConstrt = 500;
 var fixFee = 50;
-var finalFee;
 
-window.onload = function()
+window.onload = function()  // sets focus on first box, submit button
 {
     document.getElementById("resplum").focus();
     document.getElementById("sbmt").onclick = getValResPlumb;
 };
 
-function getValResPlumb()
+
+function getValResPlumb() //gets the value in the input box and puts it in resPlum
 {
     resPlum = document.getElementById("resplum").value;
-}
 
-function resFeeCalc()
+    document.getElementById("fixfee").value = "$" + resFeeCalc()[0];
+    document.getElementById("confee").value = "$" + resFeeCalc()[1];
+    document.getElementById("fee").value = "$" + resFeeCalc()[2];
+    document.getElementById("mainFixfee").value = "$" + (resFeeCalc()[0] * 2);
+    document.getElementById("mainconfee").value = "$" + (resFeeCalc()[1] * 2);
+    document.getElementById("mainfee").value = "$" + (resFeeCalc()[2] * 2);
+
+};
+
+function resFeeCalc() //calculates the fees and returns as array 
 {
-    var fixtureFee = resPlum * fixFee;
-    var conFee = (resPlum * costConstrt) / 1000
-}
+    if (resPlum != 0){
+        var fixtureFee = resPlum * fixFee; //calculates the fee for the fixtures
+        var conFee = Math.ceil(((resPlum * costConstrt + 500) / 1000)) * costConstructfee; // calculates the cost of construction the 500 is for the first fixture being $1000 cost
+        var totalFee = conFee + fixtureFee; //total fee
+
+    } else{
+        return [0, 0, 0]
+    }
+    return [fixtureFee, conFee, totalFee];
+};
 
